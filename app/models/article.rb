7e07db77389608aa6c56a677fb4aed1e9f64bebc
shabbir <ssaifee@covermymeds.com>
2018-03-04 04:49:36 -0500

    added association between user and articles and debug view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 
 class Article < ActiveRecord:: Base
-  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  belongs_to :user
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true
+  validates :user_id, presence: true
 end
