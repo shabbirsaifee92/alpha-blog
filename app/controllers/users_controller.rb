e3792f67934444b6a363b11d5a484979e4fd476b
shabbir <ssaifee@covermymeds.com>
2018-03-04 06:19:54 -0500

    create show for users and gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
