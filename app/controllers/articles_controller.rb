082cf3a595a5f0e771f2f51d6be53f0623209611
shabbir <ssaifee@covermymeds.com>
2018-03-05 16:25:34 -0500

    complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@ class ArticlesController < ApplicationController
 
 
   before_action :set_article, only: [:update, :edit, :destroy, :show]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -13,7 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    #@article.user = User.first
     if @article.save
       flash[:success] = 'New article successfully created'
       redirect_to article_path(@article)
@@ -24,7 +26,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
@@ -58,5 +59,11 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = 'You can only edit and delete your own article'
+      redirect_to root_path
+    end
+  end
 
 end
