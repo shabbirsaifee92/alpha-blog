6ce1f984c889cd76d0b76a358a07ddedab0bc163
shabbir <ssaifee@covermymeds.com>
2018-03-05 10:41:32 -0500

    added pagination to view

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'bootstrap-sass', '~> 3.3.5'
 gem 'rails', '~> 5.1.5'
 # Use sqlite3 as the database for Active Record
 gem 'bcrypt', '~> 3.1.7'
+gem 'will_paginate', '3.1.5'
+gem 'bootstrap-will_paginate', '1.0.0'
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:update, :edit, :destroy, :show]
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -12,9 +12,8 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    debugger
     @article = Article.new(article_params)
-    #@article.user = User.first
+    @article.user = User.first
     if @article.save
       flash[:success] = 'New article successfully created'
       redirect_to article_path(@article)
